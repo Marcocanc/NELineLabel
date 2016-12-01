fcf1fc43e0c541b324cc008d2487cbab87378ab4
Marco Cancellieri <marco@cancellieri.de>
2016-12-01 14:35:09 +0100

    carthage support

---
+++ NELineLabel/NELineLabel.h
@@ -0,0 +1,19 @@
+//
+//  NELineLabel.h
+//  NELineLabel
+//
+//  Created by Marco Cancellieri on 01/12/2016.
+//  Copyright © 2016 Marco Cancellieri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NELineLabel.
+FOUNDATION_EXPORT double NELineLabelVersionNumber;
+
+//! Project version string for NELineLabel.
+FOUNDATION_EXPORT const unsigned char NELineLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NELineLabel/PublicHeader.h>
+
+
